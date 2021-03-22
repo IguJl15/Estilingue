96696c463e9d775ada9f52688a8043e32dc4129e
Igor Julliano <igor.julliano3@gmail.com>
2021-03-22 00:41:36 -0300

    Adicionar arquivos de projeto.

---
+++ Estilingue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estilingue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
